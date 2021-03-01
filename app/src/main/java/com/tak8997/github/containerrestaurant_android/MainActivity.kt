2b4689f5e9f2bb426bcee5ec8d5f46a15bbaad09
kyle lee <kyle.lee@balancehero.com>
2021-03-01 18:29:52 +0900

    린트, 코드스타일 구성

--- app/src/main/java/com/tak8997/github/containerrestaurant_android/MainActivity.kt
+++ app/src/main/java/com/tak8997/github/containerrestaurant_android/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tak8997.github.containerrestaurant_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
