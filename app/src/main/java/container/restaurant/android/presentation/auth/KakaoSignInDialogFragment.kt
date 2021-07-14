1b5bbf6db6ca6003d5d453e2d7ce8d2be0bed01d
dEpayse <cbs5295@gmail.com>
2021-07-14 20:50:22 +0900

    fix : KakaoSignInDialogFragment 뷰모델 변경
    HomeViewModel -> AuthViewModel로 변경

--- app/src/main/java/container/restaurant/android/presentation/auth/KakaoSignInDialogFragment.kt
+++ app/src/main/java/container/restaurant/android/presentation/auth/KakaoSignInDialogFragment.kt
@@ -11,7 +11,6 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
 import container.restaurant.android.R
 import container.restaurant.android.databinding.FragmentKakaoSigninBinding
-import container.restaurant.android.presentation.home.HomeViewModel
 import container.restaurant.android.presentation.user.UserProfileActivity
 import container.restaurant.android.util.DataTransfer
 import container.restaurant.android.util.EventObserver
@@ -23,7 +22,7 @@ import timber.log.Timber
 internal class KakaoSignInDialogFragment : DialogFragment() {
 
     private lateinit var binding: FragmentKakaoSigninBinding
-    private val viewModel: HomeViewModel by sharedViewModel()
+    private val viewModel: AuthViewModel by sharedViewModel()
 
     private lateinit var provider: String
     private lateinit var accessToken: String
@@ -82,25 +81,21 @@ internal class KakaoSignInDialogFragment : DialogFragment() {
     }
 
     private fun observeData() {
-        with(viewModel){
+        with(viewModel) {
             observe(signInWithAccessTokenResult) { response ->
                 response.id
             }
             signInWithAccessTokenSuccess.observe(this@KakaoSignInDialogFragment, EventObserver {
-                if(it) {
-                    startActivity(UserProfileActivity.getIntent(requireContext()))
-                    this@KakaoSignInDialogFragment.dismiss()
-                }
+                startActivity(UserProfileActivity.getIntent(requireContext()))
+                this@KakaoSignInDialogFragment.dismiss()
             })
             notOurUser.observe(this@KakaoSignInDialogFragment, EventObserver {
-                if(it) {
-                    val intent = SignUpActivity.getIntent(requireContext()).apply {
-                        putExtra(DataTransfer.PROVIDER, provider)
-                        putExtra(DataTransfer.ACCESS_TOKEN, accessToken)
-                    }
-                    startActivity(intent)
-                    this@KakaoSignInDialogFragment.dismiss()
+                val intent = SignUpActivity.getIntent(requireContext()).apply {
+                    putExtra(DataTransfer.PROVIDER, provider)
+                    putExtra(DataTransfer.ACCESS_TOKEN, accessToken)
                 }
+                startActivity(intent)
+                this@KakaoSignInDialogFragment.dismiss()
             })
             errorMessageId.observe(this@KakaoSignInDialogFragment, EventObserver {
                 Toast.makeText(requireContext(), getString(it), Toast.LENGTH_LONG).show()
